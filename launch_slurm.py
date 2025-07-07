560a892c3bc507ff995969763e0404a9a2534915
Italo Amaya <italoamaya@me.com>
2025-07-07 18:20:44 +0200

    using optuna to optimize over a more params

--- launch_slurm.py
+++ launch_slurm.py
@@ -9,59 +9,66 @@ PROJECT = f"hydra-sweeps-{SWEEP_CONFIG}"
 # Parameters that represent each unique optimisation space
 corruption_types = ["identity", "full_dense", "block_diagonal"]
 optimizers = ["md", "gd"]
+alphas = [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99]
 
 for corruption in corruption_types:
     for opt in optimizers:
-        study_name = f"study_{corruption}_{opt}"
-        group = f"{corruption}_{opt}"
-        name = f"{corruption}_{opt}"
-        
-        # Create the batch script as a multi-line string
-        template_script = dedent(f"""\
-            #!/bin/bash
-            #SBATCH --job-name={name}
-            #SBATCH --output=slurm-logs/{PROJECT}/{name}_%j.out
-            #SBATCH --error=slurm-logs/{PROJECT}/{name}_%j.err
-            #SBATCH --time=4:00:00
-            #SBATCH --partition=gpu
-            #SBATCH --gres=gpu:1
-            #SBATCH --mem=16G
-            #SBATCH --cpus-per-task=4
+        for alpha in alphas:
+            study_name = f"study_{corruption}_{opt}_{alpha}"
+            group = f"{corruption}_{opt}_{alpha}"
+            name = f"{corruption}_{opt}_{alpha}"
 
-            module load miniforge
-            conda activate $HOME/{CONDA_ENV_NAME}
+            # Create the batch script as a multi-line string
+            template_script = dedent(f"""\
+                #!/bin/bash
+                #SBATCH --job-name={name}
+                #SBATCH --output=slurm-logs/{PROJECT}/{name}_%j.out
+                #SBATCH --error=slurm-logs/{PROJECT}/{name}_%j.err
+                #SBATCH --time=4:00:00
+                #SBATCH --partition=gpu
+                #SBATCH --gres=gpu:1
+                #SBATCH --mem=16G
+                #SBATCH --cpus-per-task=4
 
-            export CUDA_DEVICE_ORDER=PCI_BUS_ID
+                module load miniforge
+                conda activate $HOME/{CONDA_ENV_NAME}
 
-            LOGGING="$SCRATCH/{PROJECT}/{study_name}"
+                export CUDA_DEVICE_ORDER=PCI_BUS_ID
 
-            mkdir -p "$LOGGING"
-            CHKP="$LOGGING/last.ckpt"
+                LOGGING="$SCRATCH/{PROJECT}/{study_name}"
 
-            cd {REPO_DIR}
-            echo "Copying data from {REPO_DIR}/data into $TMP_SHARED/data"
-            cp -r "{REPO_DIR}/data" "$TMP_SHARED/data"
+                mkdir -p "$LOGGING"
+                CHKP="$LOGGING/last.ckpt"
 
-        """)
+                cd {REPO_DIR}
+                echo "Copying data from {REPO_DIR}/data into $TMP_SHARED/data"
+                cp -r "{REPO_DIR}/data" "$TMP_SHARED/data"
 
-        cmd = [
-            "python", "src/train.py", "-m", 
-            f"corruption.corruption_type={corruption}",
-            f"optimizer.update_alg={opt}",
-            f"hydra.sweeper.study_name={study_name}",
-            f"logger.group={group}",
-            f"hparams_search={SWEEP_CONFIG}",
-            f"logger.project={PROJECT}"
-        ]
+            """)
 
-        # Add the command to run the script
-        batch_script = template_script + "\n" + " ".join(cmd) + "\n" + "echo 'Job completed.'\n"
+            cmd = [
+                "python", "src/train.py", "-m", 
+                f"corruption.corruption_type={corruption}",
+                f"optimizer.update_alg={opt}",
+                f"optimizer.alpha={alpha}",
+                f"hydra.sweeper.study_name={study_name}",
+                f"logger.group={group}",
+                f"hparams_search={SWEEP_CONFIG}",
+                f"logger.project={PROJECT}"
+            ]
 
-        # Write the script to a temp file (can be named uniquely)
-        script_filename = f"tmp.sh"
+            if opt == "md":
+                # disable wd for mirror descent
+                cmd.append(f"hydra.sweeper.params.optimizer.weight_decay=choice(0)")
 
-        with open(script_filename, "w") as f:
-            f.write(batch_script)
+            # Add the command to run the script
+            batch_script = template_script + "\n" + " ".join(cmd) + "\n" + "echo 'Job completed.'\n"
 
-        # Launch the job using sbatch
-        subprocess.run(["sbatch", script_filename])
+            # Write the script to a temp file (can be named uniquely)
+            script_filename = f"tmp.sh"
+
+            with open(script_filename, "w") as f:
+                f.write(batch_script)
+
+            # Launch the job using sbatch
+            subprocess.run(["sbatch", script_filename])
