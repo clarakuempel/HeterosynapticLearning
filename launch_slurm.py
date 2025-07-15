8a24c5a9559552f708e5ec720da07a65b57e18f5
Italo Amaya <italoamaya@me.com>
2025-07-15 07:01:12 +0100

    better launcher

--- launch_slurm.py
+++ launch_slurm.py
@@ -1,5 +1,6 @@
 import subprocess
 from textwrap import dedent
+from itertools import product
 import os
 CONDA_ENV_NAME = "HL-env"
 REPO_DIR = os.path.abspath(".")  # adjust if needed
@@ -7,22 +8,33 @@ SWEEP_CONFIG = "grid"
 PROJECT = f"rerun-corruption-{SWEEP_CONFIG}"
 
 # Parameters that represent each unique optimisation space
-# corruption_types = ["identity", "full_dense", "block_diagonal"]
-corruption_types = ["block_diagonal", "full_dense"]
-alphas = [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99] # 8
-lrs = [0.001, 0.01, 0.05, 0.1, 0.5, 1.0] # 7
-optimizers = ['gd', 'md'] # 2
-weight_decays = [0.0001, 0.001, 0.01] # 5
+grid = {
+    "default": {
+        "corruption.corruption_type": ["block_diagonal", "full_dense"],
+        "optimizer.lr": [0.001, 0.01, 0.05, 0.1, 0.5, 1.0],
+    },
+        
+    "md": {
+        "optimizer.update_alg": ['md'],
+        "optimizer.block_size": [2, 4, 6, 8],
+        "optimizer.alpha": [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99],
+    },
+    "gd": {
+        "optimizer.update_alg": ['gd'],
+        "optimizer.weight_decay": [0.0001, 0.001, 0.01],
+    }
+}
+    
 
-
-# TODO This can be made cleaner and more elegant with kwargs
-def launch_job(opt, lr, alpha, weight_decay):
+def launch_job(**hp):
     """
     Launch a job on SLURM with the specified parameters.
+
+    args == hyper params
     """
-    study_name = f"study_{lr}_{opt}_{alpha}"
-    group = f"{lr}_{opt}_{alpha}"
-    name = f"{lr}_{opt}_{alpha}"
+    name = "_".join([str(hp[k]) for k in sorted(hp)])
+    study_name = f"study_{name}"
+    group = name
 
     data_dir = "$TMP_SHARED"
     # Create the batch script as a multi-line string
@@ -55,12 +67,6 @@ def launch_job(opt, lr, alpha, weight_decay):
 
     cmd = [
         "python", f"{REPO_DIR}/src/train.py", "-m", 
-
-        f"optimizer.lr={lr}",
-        f"optimizer.update_alg={opt}",
-        f"optimizer.alpha={alpha}",
-        f"optimizer.weight_decay={weight_decay}",
-
         f"hydra.sweeper.study_name={study_name}",
         f"hparams_search={SWEEP_CONFIG}",
         f"logger.group={group}",
@@ -68,6 +74,10 @@ def launch_job(opt, lr, alpha, weight_decay):
         f"logger.project={PROJECT}",
         f"data.data_dir={data_dir}/data",
     ]
+
+    # the keu is the name of the hyperparameter, the value is the value to set it to
+    for key, value in hp.items():
+        cmd.append(f"{key}={value}")
 
     # Add the command to run the script
     batch_script = template_script + "\n" + " ".join(cmd) + "\n" + "echo 'Job completed.'\n"
@@ -82,11 +92,19 @@ def launch_job(opt, lr, alpha, weight_decay):
     subprocess.run(["sbatch", script_filename])
 
 
-for opt in optimizers:
-    for lr in lrs:
-        if opt == 'md':
-            for alpha in alphas:
-                launch_job(opt, lr, alpha, 0.0)
-        else:  # 'gd'
-            for wd in weight_decays:
-                launch_job(opt, lr, 0.0, wd)
+for space, params in grid.items():
+    if space == "default":
+        continue
+
+    # Add the default parameters to the grid
+    full_params = {**grid["default"], **params}
+    keys = sorted(full_params.keys())
+    values_list = [full_params[key] for key in keys]
+
+    for values in product(*values_list):
+        hp = {
+            key: value
+            for key, value in zip(keys, values)
+        }
+        # Launch the job with the hyperparameters
+        launch_job(**hp)
