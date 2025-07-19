82b629ae4957008bb65f0a1ff7001e26d160ad12
Italo Amaya <italoamaya@me.com>
2025-07-19 19:09:40 +0200

    seeds for the corruption grid with 30 epochs

--- launch_slurm.py
+++ launch_slurm.py
@@ -5,24 +5,28 @@ import os
 CONDA_ENV_NAME = "HL-env"
 REPO_DIR = os.path.abspath(".")  # adjust if needed
 SWEEP_CONFIG = "grid"
-PROJECT = f"hydra-sweeps-{SWEEP_CONFIG}"
+PROJECT = f"seeds-corruption-{SWEEP_CONFIG}"
 
 # Parameters that represent each unique optimisation space
 grid = {
     "default": {
         "corruption.corruption_type": ["identity", "block_diagonal", "full_dense"],
-        # NOTE: Lr is searched in the grid.yaml file as it is global
-        # "optimizer.lr": [0.001, 0.01, 0.05, 0.1, 0.5, 1.0],
+        "optimizer.lr": [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 2.0],
+        "trainer.max_epochs": [20, 30],
     },
-    # "md": {
-        # "optimizer.update_alg": ['md'],
-        # "optimizer.block_size": [2, 4, 6, 8],
-        # "optimizer.alpha": [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99],
-    # },
-    # "gd": {
-        # "optimizer.update_alg": ['gd'],
-        # "optimizer.weight_decay": [0.0001, 0.001, 0.01, 0.0],
-    # },
+    "md": {
+        "optimizer.update_alg": ['md'],
+        "optimizer.block_size": [2, 4, 6, 8],
+        "optimizer.alpha": [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99],
+    },
+    "gd": {
+        "optimizer.update_alg": ['gd'],
+        "optimizer.weight_decay": [0.0001, 0.001, 0.01, 0.0],
+    },
+    "adam": {
+        "optimizer.update_alg": ['adam'],
+        "optimizer.weight_decay": [0.0001, 0.001, 0.01, 0.0],
+    }
     "adamW": {
         "optimizer.update_alg": ['adamW'],
         "optimizer.weight_decay": [0.0001, 0.001, 0.01, 0.0],
@@ -95,7 +99,33 @@ def launch_job(**hp):
     # Launch the job using sbatch
     subprocess.run(["sbatch", script_filename])
 
+def print_grid_stats(grid):
+    default = grid.get("default", {})
+    total = 0
 
+    print("Grid Search Stats:\n")
+
+    for space, params in grid.items():
+        if space == "default":
+            continue
+
+        # Merge default with specific subspace params
+        full_params = {**default, **params}
+        keys = sorted(full_params.keys())
+        values_list = [full_params[key] for key in keys]
+
+        num_configs = 1
+        for v in values_list:
+            num_configs *= len(v)
+
+        print(f"  - {space}: {num_configs} configurations")
+        total += num_configs
+
+    print(f"\nTotal configurations: {total}")
+
+
+print_grid_stats(grid)
+input("Press Enter to continue... or Ctrl+C to exit.")
 for space, params in grid.items():
     if space == "default":
         continue
