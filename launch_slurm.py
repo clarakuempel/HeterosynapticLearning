b884e487b5e2c33aa188907d5466566879d41ece
Italo Amaya <italoamaya@me.com>
2025-08-01 16:57:50 +0900

    grid over the momentum

--- launch_slurm.py
+++ launch_slurm.py
@@ -18,7 +18,7 @@ grid = {
         "optimizer.update_alg": ['md'],
         "optimizer.block_size": [2, 4, 6, 8],
         "optimizer.alpha": [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99],
-        "optimizer.momentum": [0.9],
+        "optimizer.momentum": [0.0, 0.9, 0.95, 0.99],
     },
     "adam": {
         "optimizer.update_alg": ['adam'],
