2e1c4c59a117a10852997723128f94c2cce6eb94
Italo Amaya <italoamaya@me.com>
2025-08-08 16:39:37 +0900

    adamW run

--- launch_slurm.py
+++ launch_slurm.py
@@ -12,15 +12,10 @@ data = False # add the data param?
 # Parameters that represent each unique optimisation space
 grid = {
     "default": {
-        "optimizer.lr": [0.001, 0.01, 0.05, 0.1, 0.5, 1.0, 2.0],
+        "optimizer.lr": [0.01],
     },
-    "md": {
-        "optimizer.update_alg": ['md'],
-        "optimizer.block_size": [2, 4, 6, 8],
-        "optimizer.alpha": [0.01, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.99],
-    },
-    "gd": {
-        "optimizer.update_alg": ['gd'],
+    "adamW": {
+        "optimizer.update_alg": ['adamW'],
     },
 }
     
